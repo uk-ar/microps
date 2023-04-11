a3c8895dc63f7d80799146ae1330bedfd5451e04
Yuuki Arisawa <yuuki.ari@gmail.com>
2023-04-11 08:40:02 +0000

    Enable test

--- build.zig
+++ build.zig
@@ -5,23 +5,30 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    //const lib = b.addStaticLibrary("microps", "test/test.zig");
     const lib = b.addStaticLibrary("microps", null);
-    lib.linkSystemLibrary("c");
-    lib.linkLibC();
-    lib.addIncludePath("./");
-    lib.addCSourceFile("util.c", &[_][]const u8{""});
     lib.setBuildMode(mode);
-
+    //lib.addCSourceFile("net.c", &[_][]const u8{"-std=c99"});
+    lib.addCSourceFile("net.c", &[_][]const u8{""});
+    lib.addCSourceFile("util.c", &[_][]const u8{""});
+    lib.addCSourceFile("ip.c", &[_][]const u8{""});
+    lib.addCSourceFile("udp.c", &[_][]const u8{""});
+    lib.addCSourceFile("arp.c", &[_][]const u8{""});
+    lib.addCSourceFile("ether.c", &[_][]const u8{""});
+    lib.addCSourceFile("icmp.c", &[_][]const u8{""});
+    lib.addCSourceFile("tcp.c", &[_][]const u8{""});
+    lib.addCSourceFile("driver/dummy.c", &[_][]const u8{""});
+    lib.addIncludePath("./");
+    lib.addCSourceFile("./platform/linux/intr.c", &[_][]const u8{""});
+    lib.addCSourceFile("./platform/linux/sched.c", &[_][]const u8{""});
+    lib.linkLibC();//for stdio.h
+    lib.addIncludePath("./platform/linux");
     lib.install();
 
     const main_tests = b.addTest("test/test.zig");
-    main_tests.addIncludePath("./");
-    main_tests.linkSystemLibrary("c");
-    main_tests.linkLibrary(lib);
-    //main_tests.linkSystemLibrary("microps");
-    main_tests.linkLibC();
+    main_tests.step.dependOn(&lib.step);
     main_tests.setBuildMode(mode);
+    main_tests.linkLibrary(lib);
+    main_tests.addIncludePath("./");
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
--- test/test.zig
+++ test/test.zig
@@ -1,24 +1,33 @@
 // zig test -freference-trace test.zig -lc -I ./
 const std = @import("std");
-const assert = std.debug.assert;
 const c = @cImport({
-    @cInclude("util.c");
-    @cInclude("stdio.h");
-    //@cInclude("net.c");
+    @cInclude("util.h");
+    @cInclude("net.h");
+    @cInclude("tcp.h");
+    @cInclude("driver/dummy.h");
+    @cInclude("test/test.h");
 });
-//const c = @cImport(@cInclude("util.c"));
 const testing = std.testing;
 
-test "integer overflow at compile time" {
-    const x: u8 = 255;
-    assert(x == 255);
-    try testing.expectEqual(@as(i32, 1234), c.byteorder());
-    try testing.expectEqual(@as(i32, 10), c.add(3, 7));
-    _ = c.printf("Hello, C!\n");
+test "basic add functionality" {
+    //try testing.expect(add(3, 7) == 10);
+    //try testing.expect(c.byteorder() == 1234);
+    try testing.expectEqual(c.hton16(10),2560);
 }
 
-test "step1" {
+test "step1"{
+    try testing.expect(c.net_init()!=-1);
+    const dev = c.dummy_init();
+    try testing.expect(dev!=null);
+    try testing.expect(c.net_run() != -1);
+    try testing.expect(c.net_device_output(dev, 0x800, &c.test_data, c.test_data.len, null) != -1);
+    //assert transmitted value at dummy_transmit
+    try testing.expect(c.net_device_output(dev, 0x800, &c.test_data, c.test_data.len, null) != -1);
+    c.net_shutdown();
+}
+
+test "step2" {
     //try testing.expectEqual(@as(i32, 1234), c.net_init());
-    try testing.expectEqual(@as(i32, 10), c.add(3, 7));
+    //try testing.expectEqual(@as(i32, 10), c.add(3, 7));
     _ = c.printf("Hello, C!\n");
 }
